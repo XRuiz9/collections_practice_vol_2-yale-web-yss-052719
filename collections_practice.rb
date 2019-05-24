1bcab30276e9bf9425637e208d6e04decd542685
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-24 21:55:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -73,7 +73,7 @@ def find_cool (arg)
   arg.each do |person|
     person.each do |key, value|
       if key == :temperature && value = :cool
-        name = person[:name]
+        name = person
       end
     end
   end
