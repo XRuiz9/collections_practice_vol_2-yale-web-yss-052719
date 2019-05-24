a772f6d096d466218db3bdb363bdc2d5a30ec992
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-24 22:30:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,7 +58,7 @@ def merge_data (data1, data2)
       data2.each do |person2|
         person2.each do |key2, value2|
           if key == :first_name && value == key2
-            data1[0] = data1[0].merge(value2)
+            person = person.merge(value2)
           end
         end
       end
